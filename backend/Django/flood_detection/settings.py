31416c8291b6be5f4b201876271041f553152f44
teakaiii <jennypadillo90@gmail.com>
2026-09-22 13:56:24 +0800

    Update ALLOWED_HOSTS configuration

--- backend/Django/flood_detection/settings.py
+++ backend/Django/flood_detection/settings.py
@@ -2,6 +2,7 @@
 Django settings for AHON FloodWatch project.
 """
 
+import os
 import sys
 from pathlib import Path
 from datetime import timedelta
@@ -24,10 +25,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-produc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1,aptitude-unpopular-demotion.ngrok-free.dev'
-).split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
